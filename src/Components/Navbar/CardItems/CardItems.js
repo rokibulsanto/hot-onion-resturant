dc2f81cda3901f2645597698398928e2328dae9d
rokibulsanto <rhsanto360@gmail.com>
2020-09-16 00:41:44 +0600

    hot onion resturant comming soon...

--- src/Components/Navbar/CardItems/CardItems.js
+++ src/Components/Navbar/CardItems/CardItems.js
@@ -3,45 +3,20 @@ import { Card, CardDeck } from 'react-bootstrap';
 
 const CardItems = (props) => {
   console.log(props.foods);
-    const {image,title,text} = props.foods;
+    const {image,title,text,price} = props.foods;
   
     return (
-         <div>
-        <CardDeck>
-            <Card>
+
+            <Card style={{width:'25%',float:'left',margin:'10px 3%'}}>
               <Card.Img variant="top" src={image} />
               <Card.Body>
               <Card.Title>{title}</Card.Title>
                 <Card.Text>
                   {text}
                 </Card.Text>
-                
+                <h4>Price:$ {price}</h4>
               </Card.Body>
             </Card>
-
-                <Card>
-                  <Card.Img variant="top" src={image} />
-                  <Card.Body>
-                    <Card.Title>{title}</Card.Title>
-                    <Card.Text>
-                    {text}
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-
-                <Card>
-                  <Card.Img variant="top" src={image} />
-                  <Card.Body>
-                    <Card.Title>{title}</Card.Title>
-                    <Card.Text>
-                    {text}
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-
-       </CardDeck>
-       
-</div>
     );
 };
 
--- src/Components/Navbar/Home/Home.js
+++ src/Components/Navbar/Home/Home.js
@@ -1,23 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { CardDeck } from 'react-bootstrap';
 import CardItems from '../CardItems/CardItems';
 import Data from '../FakeData/Data';
 
 const Home = () => {
-    const [allFood,setAllFood] = useState([Data]);
+    const [allFood,setAllFood] = useState(Data);
     console.log(allFood);
-    
+    const [category,setCategory] = useState('Lunch');
+    useEffect(()=> {
+       const filterData =  Data.filter(item => item.category === category)
+       setAllFood(filterData);
+    },[category])
    
     return (
         <div>
-            <div style={{textAlign:'center'}}>
-                <button>Breakfast</button>
-                <button>Lunch</button>
-                <button>Dinner</button>
+            <div style={{textAlign:'center',margin:'0px 6px 2px 2px'}}>
+                <button onClick={()=> setCategory('Breakfast')}>Breakfast</button>
+                <button onClick={()=> setCategory('Lunch')}>Lunch</button>
+                <button onClick={()=> setCategory('Dinner')}>Dinner</button>
             </div>
+            <div className="row">
             
             {
                 allFood.map(fd => <CardItems foods ={fd}></CardItems>)
             }
+            </div>
+           
         </div>
     );
 };
